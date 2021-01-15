88b506cbbbf6b6fd3c6cf7fc798aa2f98f5bf9e4
lTheCrow <ps2biciops2megabicio@gmail.com>
2021-01-15 19:47:58 -0300

    Chapter 1 - Exercise 3

---
+++ Chapter 1 - A Tutorial Introduction/Exercise 3/tempconvert.c
@@ -0,0 +1,28 @@
+/**
+ * tempconvert.c 
+ * 
+ * Modify the temperature conversion program to print a heading above the table.
+ * 
+ */
+#include <stdio.h>
+
+int main(){
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;          /* lower limit of temperatuire scale */ 
+    upper = 500;        /* upper limit */ 
+    step = 20;           /* step size */
+
+    printf("\n");
+    printf("------------------------------------\n");
+    printf("|    Celsius    |    Fahrenheit    |\n");
+    printf("------------------------------------\n");
+    fahr = lower;
+    while(fahr <= upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("|\t%3.0f\t| \t%6.1f\t   |\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    printf("------------------------------------\n");
+}
